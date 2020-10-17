993ab55150dafc30cdbccd6cb9ef2e5fa8d6613a
Petr Filatov <pmfilatov@gmail.com>
2020-10-17 17:09:42 +0300

    manager added

---
+++ src/main/java/domain/GeoInfo.java
@@ -0,0 +1,7 @@
+package domain;
+
+public class GeoInfo {
+    private String type;
+    private String coordinates;
+    private  String place;
+}
---
+++ src/main/java/manager/PostManager.java
@@ -0,0 +1,12 @@
+package manager;
+
+import domain.Post;
+
+public class PostManager {
+    public void delete(int postId) {
+    }
+
+    public Post[] search(int ownerWallId, int data, String text) {
+        return null;
+    }
+}
